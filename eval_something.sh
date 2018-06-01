3fb036e2b7fc82e64bf2eeabb53e872b6e3a5785
Xinqi Zhu <zhuxinqimac@yahoo.com>
2018-06-01 18:02:32 +1000

    modify eval_something

--- eval_something.sh
+++ eval_something.sh
@@ -2,14 +2,13 @@ CUDA_VISIBLE_DEVICES=1 python main.py --root_path \
     /home/xinqizhu/repo/3D-ResNets-PyTorch --video_path \
     /home/xinqizhu/Something_frames --annotation_path \
     /home/xinqizhu/repo/3D-ResNets-PyTorch/annots/something.json \
-    --result_path Something_results/results_34_n_n --dataset something \
+    --result_path Something_results/test --dataset something \
     --n_classes 174 --n_finetune_classes 174 --pretrain_path \
-    Something_results/results_34_s_s/save_50.pth \
+    Something_results/vanilla_3d_something/save_45.pth \
     --ft_begin_index 3 \
     --model resnet --model_depth 34 --resnet_shortcut A \
     --batch_size 16 --n_threads 2 --checkpoint 5 \
     --sample_duration 32 --resume_path \
-    Something_results/results_34_s_s/save_50.pth \
+    Something_results/vanilla_3d_something/save_45.pth \
     --sample_size 84 \
-    --no_train --no_val --test \
-    --test_shuffle
+    --no_train --no_val --test
